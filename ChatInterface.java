eb089bccd86916a1b3512b015d935bc9cbdcbfe5
Amy Chan <39275403+achanchan@users.noreply.github.com>
2020-02-18 15:42:58 -0500

    Delete ChatInterface.java

--- ChatInterface.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ChatInterface extends Remote{
-  public String helloTo(String name) throws RemoteException;
-  public String addClient(String username) throws RemoteException;
-  public String list() throws RemoteException;
-}
